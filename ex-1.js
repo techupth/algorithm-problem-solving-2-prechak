a2f1fc97da4917375b2cd3892a106159a89beb09
Precha Krasaesup <main@Prechas-Mac-Pro.local>
2024-07-17 15:08:38 +0700

    resolve algo problem ex1 and ex2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,14 @@
+// เริ่มเขียนโค้ดตรงนี้จ้า
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < students.length; i++) {
+    if (searchStudent === students[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 // ตอบคำถามตรงนี้จ้า
+/*
+BigO เท่ากับ O(n^1) หรือ O(n) เพราะ Loop แค่รอบเดียวเป็นการหาแบบ LinearSearch
+*/
